30531702a2078227f18753378d97c2a0c03dd064
Mahmoud Elwan <mahmoudelwan460@gmail.com>
2022-06-24 14:45:06 +0200

    h

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const connectDB = require('./DB/connection');
 const app = express();
 const cors = require('cors')
+
+app.get("/" , (req,res)=>{
+  res.json({message:"Done"})
+})
 // var whitelist = ['http://example1.com', 'http://example2.com']
 // var corsOptions = {
 //   origin: function (origin, callback) {
@@ -17,16 +21,13 @@ app.use(cors())
 const port = process.env.PORT
 const indexRouter = require('./modules/index.router')
 app.use(express.json())
+
 const path = require('path')
-console.log({ dirr: __dirname });
 app.use("/api/v1/uploads", express.static(path.join(__dirname, './uploads')))
 app.use('/api/v1/auth', indexRouter.authRouter)
 app.use('/api/v1/user', indexRouter.userRouter)
 app.use('/api/v1/post', indexRouter.postRouter)
 app.use('/api/v1/admin' , indexRouter.adminRouter)
-
-connectDB()
-
 const schedule = require('node-schedule');
 const date = new Date(2022, 05, 10, 02, 27, 45)
 console.log(date);
@@ -79,4 +80,5 @@ createInvoice(invoice, path.join(__dirname, './uploads/pdf/invoice.pdf'));
 schedule.scheduleJob('40 8 17 * * 5', function(){
   console.log('The answer to life, the universe, and everything!');
 });
+connectDB()
 app.listen(port, () => console.log(`server running on port ${port}`))
